93dfdb75e0d724dccd9612a0ff4a319033a4b264
Unknown <charnwut02@gmail.com>
2018-05-10 20:42:23 +0700

    Add Constructor in User
    Add Constructor in User

--- src/main/java/camt/se234/project/entity/User.java
+++ src/main/java/camt/se234/project/entity/User.java
@@ -22,4 +22,12 @@ public class User {
     String username;
     String password;
     String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+
 }
