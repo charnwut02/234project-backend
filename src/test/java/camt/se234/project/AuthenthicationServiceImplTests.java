476ef2e58fd2448b903f9578bc96a378176c076e
Unknown <charnwut02@gmail.com>
2018-05-10 18:57:52 +0700

    Add Authentic test class
    test authentic

---
+++ src/test/java/camt/se234/project/AuthenthicationServiceImplTests.java
@@ -0,0 +1,33 @@
+package camt.se234.project;
+
+import camt.se234.project.dao.UserDao;
+import camt.se234.project.entity.User;
+import camt.se234.project.service.AuthenticationServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.mock;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class AuthenticationServiceImplTest {
+    UserDao userDao;
+    AuthenticationServiceImpl authenticationService;
+
+    @Before
+    public void setup(){
+        userDao = mock(UserDao.class);
+        authenticationService = new AuthenticationServiceImpl();
+        authenticationService.setUserDao(userDao);
+    }
+
+    @Test
+    public void authenticateTest(){
+        when(userDao.getUser("nui" ,"0000")).thenReturn(new User("nui","0000","student"));
+        assertThat(authenticationService.authenticate("nui" , "0000"), is(new User("nui","0000","student")));
+        assertThat(authenticationService.authenticate("nui" ,"0000"),is(nullValue()));
+    }
+
+
+}
